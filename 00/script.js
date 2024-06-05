9f8d9081a357f3ca9aee195d7de1b711e86f1f9c
ArthurAgomes <Arthur.agomes06@gmail.com>
2024-06-04 22:01:15 -0300

    conta-contaCorrente+contaPoupança

--- 00/script.js
+++ 00/script.js
@@ -23,11 +23,41 @@ class Conta {
     }
 }
 
-conta1 = new Conta(1001, 50);
-conta2 = new Conta(1002);
+class ContaCorrente extends Conta {
+    constructor(numero, saldo = 0, limiteChequeEspecial = 200) {
+        super(numero, saldo);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
 
-conta1.depositar(10);
-conta2.depositar(10);
+    sacar(valor) {
+        //super.sacar(valor);
+        const valorDisponivelSaque = this.saldo + this.limiteChequeEspecial;
 
-console.log(conta1.toString());
-console.log(conta2.toString());
+        if(valor > 0 && valorDisponivelSaque >= valor) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente");
+        }
+    }
+}
+
+class ContaPoupanca extends Conta {    
+    constructor(numero, saldo = 0, taxaRendimento = 0.2) {
+        super(numero, saldo)
+        this.taxaRendimento = taxaRendimento;
+    }
+
+    aplicarRendimento() {
+        console.log("Rendimento aplicado");
+    }
+}
+
+let contaC = new ContaCorrente(2001, 500);
+let contaP = new ContaPoupanca(2002, 500);
+
+console.log(contaC.toString());
+contaC.sacar(650);
+console.log(contaC.toString());
+
+contaP.aplicarRendimento();
+//contaC.aplicarRendimento(); # erro por não existir função nessa classe
