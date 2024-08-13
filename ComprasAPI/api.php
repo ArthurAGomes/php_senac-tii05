5f17dc733cca7c9034652cdcf0d94cd5175c1e70
ArthurAgomes <arthur.agomes06@gmail.com>
2024-08-13 20:24:59 -0300

    orientaçao ao objeto

--- ComprasAPI/api.php
+++ ComprasAPI/api.php
@@ -1,14 +1,17 @@
 <?php
 
 require_once 'DatabaseRepository.php';
-$database = new DatabaseRepository();
 
 $acao = $_GET['acao'];
 
 if($acao == 'listar') {
-    echo $database->getAllItems();
+    echo json_encode(DatabaseRepository::getAllItems());
 } else if($acao == 'adicionar') {
-    echo $database->addItem();
+    echo DatabaseRepository::addItem("Suco", "20");
+} else if($acao == 'atualizar') {
+    echo DatabaseRepository::updateItem(12, 1);
+}  else if($acao == 'deletar') {
+    echo json_encode(DatabaseRepository::deleteItem(10)); 
 } else {
     echo "Ação não implementada";
 }
