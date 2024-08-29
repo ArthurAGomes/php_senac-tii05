a915ae8a9b1bdabf22fd16d4bc6fbadde06e05e8
ArthurAgomes <arthur.agomes06@gmail.com>
2024-08-28 22:26:16 -0300

    pag tech

---
+++ loja_OK/conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servername ="localhost";
+$database="loja";
+$username="root";
+$password="";
+// Criar a conexao
+$conexao= mysqli_connect($servername, $username, $password, $database);
+?>
