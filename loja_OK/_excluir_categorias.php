876901aa25b2a67bc4363d4bc9c1d51543ce7ed3
ArthurAgomes <arthur.agomes06@gmail.com>
2024-09-02 19:39:10 -0300

    .

---
+++ loja_OK/_excluir_categorias.php
@@ -0,0 +1,24 @@
+<?php
+//  editar_categorias.php
+include 'menu.php';
+include 'conexao.php';
+$id_categoria = $_GET['id_categoria'];
+?>
+<html>
+<!-- editar_categorias.php -->
+
+<head>
+  <title>Editar de Categorias</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <style>
+        h3{
+            text-align: center;
+           color: blue;
+           
+        }
+     
+    </style>
+</head>
+
+<body>
